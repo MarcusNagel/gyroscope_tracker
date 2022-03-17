400a66135365463a03c986b8be735d1101e656a6
MarcusNagel <nagel_marcus@gmx.com>
2022-03-17 12:02:06 +0100

    Added function for absolute acceleration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, deprecated_member_use
+
 import 'dart:async';
+import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
 
@@ -25,6 +28,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   double x = 0, y = 0, z = 0;
+  double absAcceleration = 0;
 
   StreamSubscription? accel;
   Timer? timer;
@@ -32,7 +36,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
@@ -49,13 +52,14 @@ class _MyHomePageState extends State<MyHomePage> {
       accel?.resume();
     }
 
-    // Accelerometer events come faster than we need them so a timer is used to only proccess them every 200 milliseconds
+    // Accelerometer events come faster than we need them so a timer is used to only process them every 200 milliseconds
     if (timer == null || !timer!.isActive) {
       timer = Timer.periodic(Duration(milliseconds: 200), (_) {
         setState(() {
           x = event!.x;
           y = event!.y;
           z = event!.z;
+          absAcceleration = getAbsoluteValue();
         });
       });
     }
@@ -68,6 +72,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // set the success color and reset the count
     setState(() {});
+  }
+
+  getAbsoluteValue() {
+    double absoluteX = x.abs();
+    double absoluteY = y.abs();
+    double absoluteZ = z.abs();
+
+    absAcceleration = absoluteX + absoluteY + absoluteZ;
   }
 
   @override
@@ -98,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          "X Asis : ",
+                          "X Axis : ",
                           style: TextStyle(fontSize: 20.0),
                         ),
                       ),
@@ -106,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
                             x.toStringAsFixed(
-                                2), //trim the asis value to 2 digit after decimal point
+                                2), //trim the axis value to 2 digit after decimal point
                             style: TextStyle(fontSize: 20.0)),
                       )
                     ],
@@ -116,7 +128,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          "Y Asis : ",
+                          "Y Axis : ",
                           style: TextStyle(fontSize: 20.0),
                         ),
                       ),
@@ -124,7 +136,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
                             y.toStringAsFixed(
-                                2), //trim the asis value to 2 digit after decimal point
+                                2), //trim the axis value to 2 digit after decimal point
                             style: TextStyle(fontSize: 20.0)),
                       )
                     ],
@@ -134,7 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          "Z Asis : ",
+                          "Z Axis : ",
                           style: TextStyle(fontSize: 20.0),
                         ),
                       ),
@@ -142,7 +154,25 @@ class _MyHomePageState extends State<MyHomePage> {
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
                             z.toStringAsFixed(
-                                2), //trim the asis value to 2 digit after decimal point
+                                2), //trim the axis value to 2 digit after decimal point
+                            style: TextStyle(fontSize: 20.0)),
+                      )
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Absolute Acceleration : ",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            absAcceleration.toStringAsFixed(
+                                2), //trim the axis value to 2 digit after decimal point
                             style: TextStyle(fontSize: 20.0)),
                       )
                     ],
